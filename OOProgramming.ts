4b738530f5c1fbb9f7fb754a2daea8d3a14d3116
thongLaurent <114958055+thongLaurent@users.noreply.github.com>
2022-10-06 04:13:19 +0800

    Update OOProgramming.ts

--- OOProgramming.ts
+++ OOProgramming.ts
@@ -1,1 +1,28 @@
-/**Tao write example here */
+class Person {
+    constructor(private firstName: string,private lastName:string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    getFullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    describe(): string {
+        return `This is ${this.firstName} ${this.lastName}`;
+    }
+}
+
+class Employee extends Person {
+    constructor(
+        firstName: string,
+        lastName: string,
+        private jobTitle: string
+    ) {
+        super(firstName, lastName);
+    }
+}
+
+let employee = new Employee('john', 'Doe', 'Web developer');
+console.log(employee.getFullName);
+console.log(employee.describe());
